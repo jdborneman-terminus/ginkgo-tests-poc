e9d31d7d2e6bd327be8c4ed1d9c0fe59004bec3f
Jason Borneman <jason.borneman@terminus.com>
2022-07-22 14:58:05 -0400

    take out sleep

--- e2e/url_test.go
+++ e2e/url_test.go
@@ -2,9 +2,7 @@ package e2e
 
 import (
 	"fmt"
-	"log"
 	"net/http"
-	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -12,8 +10,6 @@ import (
 
 var _ = Describe("Try Google for a 200", func() {
 	It("should return 200", func() {
-		log.Println("------------------------>SLEEP NOW<------------------------")
-		time.Sleep(4 * time.Second)
 		resp, requestErr := http.Get(fmt.Sprintf("https://%s", baseURL))
 		Expect(requestErr).To(BeNil())
 		Expect(resp.StatusCode).To(Equal(200))
