bce2ca4c7c6a52cf97b42eec6cd99b7df5582034
Jason Borneman <jason.borneman@terminus.com>
2022-07-22 14:49:26 -0400

    Make E2E (passing) finish second

--- e2e/url_test.go
+++ e2e/url_test.go
@@ -2,7 +2,9 @@ package e2e
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -10,6 +12,8 @@ import (
 
 var _ = Describe("Try Google for a 200", func() {
 	It("should return 200", func() {
+		log.Println("------------------------>SLEEP NOW<------------------------")
+		time.Sleep(4 * time.Second)
 		resp, requestErr := http.Get(fmt.Sprintf("https://%s", baseURL))
 		Expect(requestErr).To(BeNil())
 		Expect(resp.StatusCode).To(Equal(200))
